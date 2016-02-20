cfd3c9823cacbf7d4853db9e1e7f037e18aada56
Yuchen Zhou <yuczhou@mtu.edu>
2016-02-20 17:21:41 -0500

    evaluator

--- insert_frtu/evaluator.py
+++ insert_frtu/evaluator.py
@@ -9,22 +9,6 @@ logger = logging.getLogger(__name__)
 
 class SolutionEvaluator(object):
     def __init__(self, to_parent, leaf_probability):
-        self._single_evaluator = SingleSolutionEvaluator(to_parent, leaf_probability)
-
-    def evaluate(self, solutions):
-        optimal_solutions_each_level = {}
-        for number_frtus, level_solutions in groupby(solutions, key=lambda solution: solution.number_frtu):
-            evals = {solution: self._single_evaluator.evaluate(solution.frtu_list)
-                     for solution in list(level_solutions)}
-            frtus, average_leaves = min(evals.items(), key=lambda pair: pair[1])
-            optimal_solutions_each_level[number_frtus] = (frtus, average_leaves)
-            logger.info('Optimal solution with {0} FRTUs: {1}. Avg. smart meters to check: {2}'
-                        .format(number_frtus, frtus, average_leaves))
-        return optimal_solutions_each_level
-
-
-class SingleSolutionEvaluator(object):
-    def __init__(self, to_parent, leaf_probability):
         self._leaf_probability = leaf_probability
         self._to_parent = to_parent
 
@@ -41,7 +25,10 @@ class SingleSolutionEvaluator(object):
             if len(attacked_leaves) != 0:
                 covered_leaves_for_frtu = [frtu_map[leaf_to_frtu[leaf]] for leaf in attacked_leaves]
                 cases.append(sum(covered_leaves_for_frtu) / float(len(attacked_leaves)))
-        return sum(cases) / float(len(cases)) if len(cases) != 0 else -1
+        average_meters_to_check = sum(cases) / float(len(cases)) if len(cases) != 0 else -1
+        logger.info('Optimal solution with {0} FRTUs: {1}. Avg. smart meters to check: {2}'
+                    .format(len(frtu_list), frtu_list, average_meters_to_check))
+        return average_meters_to_check
 
     @staticmethod
     def _build_frtu_map(frtu_list, leaf_list, to_parent):
--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -1,52 +1,28 @@
 import unittest
 
-from mock import MagicMock
-
-from evaluator import SingleSolutionEvaluator, SolutionEvaluator
-from partial_solution import PartialSolution
+from evaluator import SolutionEvaluator
 
 
 class TestEvaluator(unittest.TestCase):
-    def setUp(self):
-        self._solution_optimal_frtu_2 = PartialSolution(frtu_list=(1, 2))
-        self._solution_suboptimal_frtu_2 = PartialSolution(frtu_list=(2, 3))
-        self._solution_optimal_frtu_3 = PartialSolution(frtu_list=(1, 2, 3))
-        self._solution_suboptimal_frtu_3 = PartialSolution(frtu_list=(2, 3, 4))
-        self._solutions = [self._solution_optimal_frtu_2,
-                           self._solution_suboptimal_frtu_2,
-                           self._solution_optimal_frtu_3,
-                           self._solution_suboptimal_frtu_3]
-        self._evaluation = dict(zip(self._solutions, [2, 2.5, 3, 3.5]))
-        self._single_evaluator = MagicMock(side_effect=self._evaluation.get)
-
-    def test_happy_path(self):
-        evaluator = SolutionEvaluator({}, {})
-        evaluator._single_evaluator = self._single_evaluator
-
-        optimal_solutions = evaluator.evaluate(self._solutions)
-        assert len(optimal_solutions) == 2
-
-
-class TestSingleEvaluator(unittest.TestCase):
     def setUp(self):
         self._to_parent = {1: 0, 2: 0, 3: 1, 5: 3, 6: 3, 7: 1, 4: 2, 8: 4, 9: 4, 10: 2}
         self._leaf_probability = {leaf: [0.5, 0.6] for leaf in [5, 6, 7, 8, 9, 10]}
 
     def test_build_frtu_map(self):
         frtu_list = [1, 2]
-        evaluator = SingleSolutionEvaluator(self._to_parent, self._leaf_probability)
+        evaluator = SolutionEvaluator(self._to_parent, self._leaf_probability)
         frtu_map, leaf_to_frtu = evaluator._build_frtu_map(frtu_list, self._leaf_probability.keys(), self._to_parent)
         assert frtu_map == {1:3, 2:3}
         assert leaf_to_frtu == {5: 1, 6: 1, 7: 1, 8: 2, 9: 2, 10: 2}
 
     def test_evaluator_happy_path(self):
         frtu_list = [1, 2]
-        evaluator = SingleSolutionEvaluator(self._to_parent, self._leaf_probability)
+        evaluator = SolutionEvaluator(self._to_parent, self._leaf_probability)
         assert evaluator.evaluate(frtu_list, 10) == 3.0
 
     def test_evaluator_no_root_frtu(self):
         frtu_list = [1]
-        evaluator = SingleSolutionEvaluator(self._to_parent, self._leaf_probability)
+        evaluator = SolutionEvaluator(self._to_parent, self._leaf_probability)
         assert evaluator.evaluate(frtu_list, 10) == 3.0
 
 
